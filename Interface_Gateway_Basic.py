4781ea0efb6a3f53259c10f11e6c022c9c21f046
Karl Latham <karl@digitalattic.net>
2016-01-19 03:07:20 +0000

    Comments and Python 3
    Added comments.
    Print statements made Python 3 compatable

--- Interface_Gateway_Basic.py
+++ Interface_Gateway_Basic.py
@@ -3,22 +3,22 @@ import time
 import sys 
 
 class MyListener(stomp.ConnectionListener): 
-    def on_error(self, headers, message): 
+    def on_error(self, headers, message):  #What to do if an error is recieved
         print('Error Received %s' % message)
-    def on_message(self, headers, message):
-        print message
-    def on_disconnected(self):
-    	print "Interface Gateway Connection Lost"
+    def on_message(self, headers, message): #What to do if a message is recieved
+        print (message)
+    def on_disconnected(self): #What to do if the interface disconnects
+    	print ("Interface Gateway Connection Lost")
 
 dest='/topic/TD_ALL_SIG_AREA' #SimSig always uses this topic!
-conn=stomp.Connection12([('127.0.0.1',51515)])
+conn=stomp.Connection12([('127.0.0.1',51515)]) #Connect to the local server on the local port
 
 conn.set_listener('test',MyListener()) 
 conn.start() 
 conn.send_frame('CONNECT',body='accept-version:1.1')
 conn.subscribe(destination=dest, id=1, ack='auto')
 
-while 1:
+while 1: #Infinite loop to keep the program alive
     time.sleep(0.01)
 
-conn.disconnect()
+conn.disconnect() #Close the gateway
